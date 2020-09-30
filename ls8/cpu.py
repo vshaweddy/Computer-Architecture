f6c115110f5aab436a93554ccdb46bb14be903c3
Vici <vshaweddy@gmail.com>
2020-09-30 18:11:06 -0400

    set up the helpers and run method

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,17 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.registers = [0] * 8 # R0-R7
+        self.pc = 0 # Program Counter (address of the currently executing instructions)
+
+    # should accept the address to read and return the value stored there
+    def ram_read(self, address):
+        return self.ram[address]
+
+    # should accept a value to write, and the address to write it to
+    def ram_write(self, value, address):
+        self.ram[address] = value
 
     def load(self):
         """Load a program into memory."""
@@ -31,35 +41,61 @@ class CPU:
             address += 1
 
 
-    def alu(self, op, reg_a, reg_b):
-        """ALU operations."""
+    # def alu(self, op, reg_a, reg_b):
+    #     """ALU operations."""
 
-        if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
-        else:
-            raise Exception("Unsupported ALU operation")
+    #     if op == "ADD":
+    #         self.reg[reg_a] += self.reg[reg_b]
+    #     #elif op == "SUB": etc
+    #     else:
+    #         raise Exception("Unsupported ALU operation")
 
-    def trace(self):
-        """
-        Handy function to print out the CPU state. You might want to call this
-        from run() if you need help debugging.
-        """
+    # def trace(self):
+    #     """
+    #     Handy function to print out the CPU state. You might want to call this
+    #     from run() if you need help debugging.
+    #     """
 
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
-            #self.fl,
-            #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
-        ), end='')
+    #     print(f"TRACE: %02X | %02X %02X %02X |" % (
+    #         self.pc,
+    #         #self.fl,
+    #         #self.ie,
+    #         self.ram_read(self.pc),
+    #         self.ram_read(self.pc + 1),
+    #         self.ram_read(self.pc + 2)
+    #     ), end='')
 
-        for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+    #     for i in range(8):
+    #         print(" %02X" % self.reg[i], end='')
 
-        print()
+    #     print()
 
     def run(self):
-        """Run the CPU."""
-        pass
+        running = True
+        pc = 0
+        
+        while running:
+            # read line by line from memory
+            instruction = self.ram_read(pc)
+            # print(instruction)
+
+            if instruction == 0b10000010:
+                reg_location = self.ram[pc + 1]
+                num = self.ram[pc +2]
+                self.ram_write(num, reg_location)
+                pc += 3
+
+            elif instruction == 0b01000111:
+                reg_location = self.ram[pc + 1]
+                print(self.ram_read(reg_location))
+                pc += 2
+            
+            elif instruction == 0b00000001:
+                running = False
+                pc += 1
+
+            # Exit if it's crashed
+            else:
+                print(f"Unknown instruction: {instruction}")
+                sys.exit(1)
+
